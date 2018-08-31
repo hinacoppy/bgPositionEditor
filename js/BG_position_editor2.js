f473d1298efd185000673ba5bde030b6f275ca7d
hinacoppy <kawakami@js4.so-net.ne.jp>
2018-08-31 13:01:54 +0900

    declare vars according to ES2015

--- js/BG_position_editor2.js
+++ js/BG_position_editor2.js
@@ -6,11 +6,12 @@ var imgpath = './img/';
 
 //入力のXGIDを解析し、結果をボードエディタ、シチュエーションエディタに反映
 function gamen_ni_hanei(xgid) {
-  xgid = (xgid) ? xgid : "XGID=--------------------------:0:0:1:00:0:0:0:0:10";
-  regexp = /XGID=(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?)$/;
-  z = xgid.match(regexp);
-  pos=z[1]; cubeval=z[2]; cubeowner=z[3]; turn=z[4]; dice=z[5];
-  sc1=z[6]; sc2=z[7]; jacoby=crawford=z[8]; matchlen=z[9]; maxcube=z[10];
+  const xgidstr = (xgid) ? xgid : "XGID=--------------------------:0:0:1:00:0:0:0:0:10";
+  const regexp = /XGID=(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?):(.+?)$/;
+  const z = xgidstr.match(regexp);
+  const pos=z[1], cubeval=z[2], cubeowner=z[3], turn=z[4], dice=z[5],
+        sc1=z[6], sc2=z[7], jacoby=crawford=z[8], matchlen=z[9], maxcube=z[10];
+  let gamemode;
 
   $('#xgid').val(xgid);
   $('#matchlength').val(matchlen);
@@ -56,14 +57,16 @@ function gamen_ni_hanei(xgid) {
   $('[name=cubevalue]').val(cubeval);
   position_ni_hanei(pos);
   calc_boff(pos);
+  gamemode_css(gamemode);
 }
 
 //ポジションの情報をボードエディタに反映
 function position_ni_hanei(position) {
-  var poslist = position.split('');
-  for (var i=0; i<poslist.length; i++) {
-    c = poslist[i];
-    pt = ("000"+i).substr(-2);
+  const poslist = position.split('');
+  let n;
+  for (let i=0; i<poslist.length; i++) {
+    const c = poslist[i];
+    const pt = ("000"+i).substr(-2);
     if (c == "-") {
       $("#n"+pt).val(0);
       if (i>=1 && i<=24) {
@@ -89,26 +92,26 @@ function position_ni_hanei(position) {
 
 //ジャコビーフラグ(クロフォードフラグ)を設定
 function check_jb_cf() {
-  gm = $('[name=gamemode]:checked').val();
-  jb = $('[name=jacoby]:checked').val();
-  cf = $('[name=crawford]:checked').val();
-  rr = (gm == 'moneygame') ? jb : 0;
-  rr = (gm == 'matchgame' && cf == 1) ? 1 : rr;
-  return rr;
+  const gm = $('[name=gamemode]:checked').val();
+  const jb = $('[name=jacoby]:checked').val();
+  const cf = $('[name=crawford]:checked').val();
+  const rr = (gm == 'moneygame') ? jb : 0;
+  const rs = (gm == 'matchgame' && cf == 1) ? 1 : rr;
+  return rs;
 }
 
 //maxcubeのキューブバリューを計算する
 function get_maxcubevaltext(maxcube) {
-  val = Math.pow(2, maxcube);
-  return '('+val+')'; 
+  return '('+Math.pow(2, maxcube)+')'; 
 }
 
 //ベアオフの数を計算・表示する。負数の場合は警告(赤文字)
 function calc_boff(position) {
-  boffw = boffb = 15;
-  var poslist = position.split('');
-  for (var i=0; i<poslist.length; i++) {
-    c = poslist[i];
+  let boffw = boffb = 15;
+  let n;
+  const poslist = position.split('');
+  for (let i=0; i<poslist.length; i++) {
+    const c = poslist[i];
     if ( c.match(/[A-Z]/) ) {
       n = c.charCodeAt(0) - "A".charCodeAt(0) + 1;
       boffb -= n;
@@ -135,11 +138,12 @@ function calc_boff(position) {
 
 //ポジションエディタの情報からposition文字列を組み立て
 function position_wo_kumitate() {
-  posstr = "";
-  for (i=0; i<26; i++) {
-    pt = ("0000"+ i).substr(-2);
-    n = Number($("#n"+pt).val());
-    col = $("[name=r"+pt+"]").val();
+  let posstr = "";
+  let ch;
+  for (let i=0; i<26; i++) {
+    const pt = ("0000"+ i).substr(-2);
+    const n = Number($("#n"+pt).val());
+    const col = $("[name=r"+pt+"]").val();
 
     if (n==0) {
       posstr += "-";
@@ -158,33 +162,33 @@ function position_wo_kumitate() {
 
 //画面のデータを元にXGIDを組み立て
 function xgidout_wo_kumitate() {
-  gamemode = $('[name=gamemode]:checked').val();
-  cubeval  = $('[name=cubevalue]').val();
-  cubeown  = $('[name=cubeowner]:checked').val();
-  turn     = $('[name=turn]:checked').val();
-  dice     = $('#dice').val();
-  sc1      = $('#score1').val();
-  sc2      = $('#score2').val();
-  jbcf     = check_jb_cf();
-  matchlen = $('#matchlength').val();
-  maxcube  = $('#maxcube').val();
-  pos      = $('#position').val();
+  const gamemode = $('[name=gamemode]:checked').val();
+  const cubeval  = $('[name=cubevalue]').val();
+  const cubeown  = $('[name=cubeowner]:checked').val();
+  const turn     = $('[name=turn]:checked').val();
+  const dice     = $('#dice').val();
+  let   sc1      = $('#score1').val();
+  let   sc2      = $('#score2').val();
+  const jbcf     = check_jb_cf();
+  let   matchlen = $('#matchlength').val();
+  const maxcube  = $('#maxcube').val();
+  const pos      = $('#position').val();
   if (gamemode == "moneygame") {
     sc1 = sc2 = matchlen = 0;
   }
-  xgidout = 'XGID='+pos+':'+cubeval+':'+cubeown+':'+turn+':'+dice+':'+sc1+':'+sc2+':'+jbcf+':'+matchlen+':'+maxcube;
+  const xgidout = 'XGID='+pos+':'+cubeval+':'+cubeown+':'+turn+':'+dice+':'+sc1+':'+sc2+':'+jbcf+':'+matchlen+':'+maxcube;
   return xgidout;
 }
 
 //JavaScript処理で、ボード情報を取得する
-function js_getboard(xgid,boardtype,imgpath) {
-  html = new HtmlBoard(xgid);
+function js_getboard(xgid, boardtype, imgpath) {
+  const html = new HtmlBoard(xgid);
   html.imgpath = imgpath;
   html.boardtype = boardtype;
-  bgboard = html.get_board_html();
-  pipinfo = html.get_pipinfo();
-  bdwidth = html.get_bdwidth();
-  $('#bgboard').show().html(bgboard).css("width",bdwidth);
+  const bgboard = html.get_board_html();
+  const pipinfo = html.get_pipinfo();
+  const bdwidth = html.get_bdwidth();
+  $('#bgboard').show().html(bgboard).css("width", bdwidth);
   $('#pipinfo').text(pipinfo);
   $("#floatWindow").css({top:320, left:600}).fadeIn("fast");
   draw_canvas();
@@ -195,10 +199,15 @@ function js_getboard(xgid,boardtype,imgpath) {
 function draw_canvas() {
   html2canvas($("#bgboard")[0], {scale:1.0}).then(function(canvas) {
     //imgタグのsrcの中に、html2canvasがレンダリングした画像を登録する。
-    var imgData = canvas.toDataURL();
+    const imgData = canvas.toDataURL();
     $("#boardone")[0].src = imgData;
     $("#downloadimg")[0].href = imgData; //ダウンロードリンクも画像にしておく
   });
+}
+
+function gamemode_css(gamemode) {
+  if (gamemode == "moneygame") { $('.deco_money').css("color", "black"); $('.deco_match').css("color", "gray"); }
+  if (gamemode == "matchgame") { $('.deco_money').css("color", "gray");  $('.deco_match').css("color", "black"); }
 }
 
 //イベントハンドラの定義
@@ -206,16 +215,16 @@ $(function() {
 
   //[Apply to Editor] ボタンがクリックされたとき
   $('#apply').on('click', function(e) {
-    xgid = $('#xgid').val();
+    const xgid = $('#xgid').val();
     gamen_ni_hanei(xgid);
   });
 
   //ゲームシチュエーションが変更されたときはxgid-outを編集する
   $('form[name="BgPositionEditor"]').on('change', function(e) {
     if (e.target.id != "xgid") {
-      pos = position_wo_kumitate();
+      const pos = position_wo_kumitate();
       $('#position').val(pos);
-      xgid = xgidout_wo_kumitate();
+      const xgid = xgidout_wo_kumitate();
       $('#xgid').val(xgid);
       calc_boff(pos);
     }
@@ -223,18 +232,18 @@ $(function() {
 
   //[Draw the Board] ボタンがクリックされたとき(CW)
   $('#draw-board').on('click', function(e) {
-    xgid = $('#xgid').val();
-    js_getboard(xgid,boardtype,imgpath); //boardtype,imgpathは広域変数から取得
+    const xgid = $('#xgid').val();
+    js_getboard(xgid, boardtype, imgpath); //boardtype,imgpathは広域変数から取得
   });
 
   //max cubeが変更されたときは倍率(キューブバリュー)を変更する
   $('#maxcube').on('change', function(e) {
-    maxcube  = $('#maxcube').val();
+    const maxcube  = $('#maxcube').val();
     $('#maxcubeval').text(get_maxcubevaltext(maxcube));
   });
 
   //[Copy to Clip] ボタンがクリックされたとき
-  var clipboard = new Clipboard('#copy2clip');
+  const clipboard = new Clipboard('#copy2clip');
   clipboard.on('success', function(e) {
     e.clearSelection();
   });
@@ -246,22 +255,21 @@ $(function() {
 
   //gamemodeが変更されたときは設定不可な項目をグレー表示に
   $('[name=gamemode]').on('change', function(e) {
-    gamemode = $('[name=gamemode]:checked').val();
-    if (gamemode == "moneygame") { $('.deco_money').css("color", "black"); $('.deco_match').css("color", "gray"); }
-    if (gamemode == "matchgame") { $('.deco_money').css("color", "gray");  $('.deco_match').css("color", "black"); }
+    const gamemode = $('[name=gamemode]:checked').val();
+    gamemode_css(gamemode);
   });
 
   //[change to (C)CW] ボタンがクリックされたとき
   $('#alt-rotation').on('click', function(e) {
-    xgid = $('#xgid').val();
-    url = (boardtype == 'gnu-cw') ? "BG_position_editor_ccw.html" : "BG_position_editor_cw.html";
+    const xgid = $('#xgid').val();
+    const url = (boardtype == 'gnu-cw') ? "BG_position_editor_ccw.html" : "BG_position_editor_cw.html";
     window.location.href = url + "?t=" + xgid;
   });
 
   //HTMLファイル読み込み時にXGIDがクエリが設定されていれば読み込み、画面に反映する
   $(window).on('load',function(){
-    ref = $(location).attr('search');
-    xgid = ref.slice(ref.indexOf('&t=XGID', 0)+4); //手抜きのクエリ判定
+    const ref = $(location).attr('search');
+    const xgid = ref.slice(ref.indexOf('&t=XGID', 0)+4); //手抜きのクエリ判定
     $('#xgid').val(xgid);
     gamen_ni_hanei(xgid); //XGIDをエディタに反映
     js_getboard(xgid,boardtype,imgpath); //ボード画像を作成
